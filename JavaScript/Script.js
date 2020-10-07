0a66fa0ee591b5d81aa887c311229e265a11aaf3
PrLamkin <shiektheimmortal@outlook.com>
2020-10-07 12:03:11 -0500

    BUTTONS

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,30 +1,31 @@
 var initialMap;
-var coords = [0,0];
-var totalWhales; 
-function initMap(species) {
-        $.ajax({
-          url: "http://hotline.whalemuseum.org/api.json?species=" + species + "&limit=1000",
-          method: "GET",
-        }).then(function(response) {
-            totalWhales = response;
-            
-        }).then(function() {
-            initialMap = new google.maps.Map(document.getElementById('map'), {
-                zoom: 3,
-                center: new google.maps.LatLng(2.8,-187.3),
-                mapTypeId: 'terrain'
-              });
-              for (var i = 0; i < totalWhales.length; i++) {
-                  coords[0] = totalWhales[i].latitude;
-                  coords[1] = totalWhales[i].longitude;
-                  var latLng = new google.maps.LatLng(coords[0],coords[1]);
-                  console.log(coords)
-                  var marker = new google.maps.Marker({position: latLng, map: initialMap });
-              }
-            });
-        }
+var coords = [0, 0];
+var totalWhales;
 
-      $("option").on("click", function(){   
-        var x = $(this).text();
-        initMap(x);
-      });
+function initMap(species) {
+    $.ajax({
+        url: "http://hotline.whalemuseum.org/api.json?species=" + species + "&limit=1000",
+        method: "GET",
+    }).then(function(response) {
+        totalWhales = response;
+
+    }).then(function() {
+        initialMap = new google.maps.Map(document.getElementById('map'), {
+            zoom: 3,
+            center: new google.maps.LatLng(2.8, -187.3),
+            mapTypeId: 'terrain'
+        });
+        for (var i = 0; i < totalWhales.length; i++) {
+            coords[0] = totalWhales[i].latitude;
+            coords[1] = totalWhales[i].longitude;
+            var latLng = new google.maps.LatLng(coords[0], coords[1]);
+            console.log(coords)
+            var marker = new google.maps.Marker({ position: latLng, map: initialMap });
+        }
+    });
+}
+
+$("option").on("click", function() {
+    var x = $(this).text();
+    initMap(x);
+});
