2b40bb9fbfe1774d037f9a2ce957c7b3cd47573e
Jordan Vang <jordanvang8@gmail.com>
2020-10-07 15:23:39 -0500

    update geocode

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -10,7 +10,7 @@ function initMap(species) {
             
         }).then(function() {
             initialMap = new google.maps.Map(document.getElementById('map'), {
-                zoom: 3,
+                zoom: 2,
                 center: new google.maps.LatLng(2.8,-187.3),
                 mapTypeId: 'terrain'
               });
@@ -19,12 +19,37 @@ function initMap(species) {
                   coords[1] = totalWhales[i].longitude;
                   var latLng = new google.maps.LatLng(coords[0],coords[1]);
                   console.log(coords)
-                  var marker = new google.maps.Marker({position: latLng, map: initialMap });
+                  var marker = new google.maps.Marker({position: latLng, map: initialMap});
               }
             });
         }
+
+        var locationLong;
+        var locationLat;
+        function spot(z){
+        $.ajax({
+          url : "https://maps.googleapis.com/maps/api/geocode/json?address=" + z +"&key=AIzaSyDvMk0f3LfIKcsMM-iJ_wkRL5DVjCTR-70",
+          method : "GET"
+        }).then(function(response){
+          locationLat = response.results[0].geometry.location.lat
+          console.log(locationLat)
+          locationLong = response.results[0].geometry.location.lng
+          console.log(locationLong)
+        })
+      }
+      spot()
+
 
       $("option").on("click", function(){   
         var x = $(this).text();
         initMap(x);
       });
+
+      $("#submit").on("click", function(event){
+        event.preventDefault();   
+        var z = $("#text").val().trim();
+        spot(z)
+      });
+
+
+
